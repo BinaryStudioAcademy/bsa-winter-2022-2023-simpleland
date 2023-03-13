eba6372b2bd85bc65b14b4c5c04d5d3d4c3d1102
Alyona Pisotska <pisotska.o@gmail.com>
2023-03-13 22:29:42 +0200

    sl-5: + general button styles

--- frontend/src/libs/components/button/button.tsx
+++ frontend/src/libs/components/button/button.tsx
@@ -1,10 +1,32 @@
+import './styles.scss';
+
 type Properties = {
   label: string;
   type?: 'button' | 'submit';
+  isFull?: boolean;
+  isSmall?: boolean;
+  disabled?: boolean;
 };
 
-const Button: React.FC<Properties> = ({ type = 'button', label }) => (
-  <button type={type}>{label}</button>
+const Button: React.FC<Properties> = ({
+  type = 'button',
+  label,
+  isFull = true,
+  isSmall = false,
+  disabled = false,
+}) => (
+  <button
+    type={type}
+    className={`
+      btn
+      button-text-2
+      ${isFull ? 'btn-empty' : 'btn-full'}
+      ${isSmall ? 'btn--small' : ''}
+    `}
+    disabled={disabled}
+  >
+    {label}
+  </button>
 );
 
 export { Button };
