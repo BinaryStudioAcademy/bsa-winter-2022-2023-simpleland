c566e150e4e4684736c028ec98b92c698c71fd46
Dima Popovych <dimapopovych.w@gmail.com>
2023-03-14 14:56:41 +0200

    sl-10: * registration dto and validation schema

--- shared/src/packages/users/libs/enums/user-validation-message.enum.ts
+++ shared/src/packages/users/libs/enums/user-validation-message.enum.ts
@@ -1,6 +1,14 @@
 const UserValidationMessage = {
   EMAIL_REQUIRE: 'Email is required',
   EMAIL_WRONG: 'Email is wrong',
+  EMAIL_IS_INVALID: 'Invalid email format',
+  PASSWORD_IS_INVALID:
+    'Password must contain 8-30 characters and can contain only letters, number and special characters',
+  PASSWORD_REQUIRE: 'Password is required',
+  FIRST_NAME_REQUIRE: 'First name is required',
+  FIRST_NAME_IS_INVALID: 'Invalid first name',
+  LAST_NAME_REQUIRE: 'Last name is required',
+  LAST_NAME_IS_INVALID: 'Invalid last name',
 } as const;
 
 export { UserValidationMessage };
