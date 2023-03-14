f44784e7c4ac4f7576c0c9c1bc437c203e673c0a
Alexandr-mas <sashamaslov591@gmail.com>
2023-03-14 22:43:08 +0200

    sl-15: + style issues

--- frontend/src/pages/auth/components/not-found/not-found.tsx
+++ frontend/src/pages/auth/components/not-found/not-found.tsx
@@ -3,24 +3,24 @@ import { useRouteError } from 'react-router-dom';
 import styless from './not-found.module.scss';
 
 interface RouteError {
-    statusText?: string;
-    message?: string;
+  statusText?: string;
+  message?: string;
 }
 
 const ErrorPage = (): JSX.Element => {
-    const error = useRouteError() as RouteError;
+  const error = useRouteError() as RouteError;
 
-    return (
-        <div className={styless.errorPage}>
-            <h1 className={styless.warningMessage}>Oops!</h1>
-            <p className={styless.text404}>
-                Sorry, an unexpected error has occurred.
-            </p>
-            <p>
-                <i className={styless.status}>{error.statusText ?? error.message}</i>
-            </p>
-        </div>
-    );
+  return (
+    <div className={styless.errorPage}>
+      <h1 className={styless.warningMessage}>Oops!</h1>
+      <p className={styless.text404}>
+        Sorry, an unexpected error has occurred.
+      </p>
+      <p>
+        <i className={styless.status}>{error.statusText ?? error.message}</i>
+      </p>
+    </div>
+  );
 };
 
 export { ErrorPage as ErrorFunction };
