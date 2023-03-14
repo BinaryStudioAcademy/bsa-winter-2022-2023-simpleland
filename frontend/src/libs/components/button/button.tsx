a0bad2ab8db769e8d0d2e8120bbeba129d4b185e
Dima Popovych <dimapopovych.w@gmail.com>
2023-03-14 12:14:46 +0200

    sl-10: * Merge with task/sl-5-sign-up

--- frontend/src/libs/components/button/button.tsx
+++ frontend/src/libs/components/button/button.tsx
@@ -1,11 +1,32 @@
+import './styles.scss';
+
 type Properties = {
   label: string;
   type?: 'button' | 'submit';
+  isFull?: boolean;
+  isSmall?: boolean;
+  disabled?: boolean;
 };
 
 const Button: React.FC<Properties> = ({
   type = 'button',
   label,
-}: Properties) => <button type={type}>{label}</button>;
+  isFull = true,
+  isSmall = false,
+  disabled = false,
+}: Properties) => (
+  <button
+    type={type}
+    className={`
+      btn
+      button-text-2
+      ${isFull ? 'btn-empty' : 'btn-full'}
+      ${isSmall ? 'btn--small' : ''}
+    `}
+    disabled={disabled}
+  >
+    {label}
+  </button>
+);
 
 export { Button };
