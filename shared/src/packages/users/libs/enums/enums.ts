29663bacdd115fcce9e1c9cc45e7437ca384d2ee
Dima Popovych <dimapopovych.w@gmail.com>
2023-03-14 17:01:31 +0200

    sl-10: + user-validation-rules enum

--- shared/src/packages/users/libs/enums/enums.ts
+++ shared/src/packages/users/libs/enums/enums.ts
@@ -1,2 +1,3 @@
 export { UserValidationMessage } from './user-validation-message.enum.js';
+export { UserValidationRules } from './user-validation-rules.enum.js';
 export { UsersApiPath } from './users-api-path.enum.js';
--- shared/src/packages/users/libs/validation-schemas/user-sign-up.validation-schema.ts
+++ shared/src/packages/users/libs/validation-schemas/user-sign-up.validation-schema.ts
@@ -1,18 +1,12 @@
 import joi from 'joi';
 
-import { UserValidationMessage } from '../enums/enums.js';
+import { UserValidationMessage, UserValidationRules } from '../enums/enums.js';
 import { type UserSignUpRequestDto } from '../types/types.js';
-
-const EMAIL_REGEX =
-  /^(\w)([\w+.-]{0,34}\w)?@[\dA-Za-z][\dA-Za-z-]{0,61}[\dA-Za-z]\.[A-Za-z]{2,}$/;
-const PASSWORD_REGEX = /^[\d!"#$%&()-_`a-z{|}~]{8,30}$/;
-const FIRST_NAME_REGEX = /^['A-Za-z-]+$/;
-const LAST_NAME_REGEX = /^['A-Za-z-]+$/;
 
 const userSignUp = joi.object<UserSignUpRequestDto, true>({
   email: joi
     .string()
-    .regex(EMAIL_REGEX)
+    .regex(UserValidationRules.EMAIL_REGEX)
     .email({
       tlds: {
         allow: false,
@@ -24,15 +18,22 @@ const userSignUp = joi.object<UserSignUpRequestDto, true>({
       'string.email': UserValidationMessage.EMAIL_WRONG,
       'string.empty': UserValidationMessage.EMAIL_REQUIRE,
     }),
-  password: joi.string().pattern(PASSWORD_REGEX).required().messages({
-    'string.pattern.base': UserValidationMessage.PASSWORD_IS_INVALID,
-    'string.empty': UserValidationMessage.PASSWORD_REQUIRE,
-  }),
-  firstName: joi.string().pattern(FIRST_NAME_REGEX).messages({
-    'string.pattern.base': UserValidationMessage.FIRST_NAME_IS_INVALID,
-    'string.empty': UserValidationMessage.FIRST_NAME_REQUIRE,
-  }),
-  lastName: joi.string().pattern(LAST_NAME_REGEX).messages({
+  password: joi
+    .string()
+    .pattern(UserValidationRules.PASSWORD_REGEX)
+    .required()
+    .messages({
+      'string.pattern.base': UserValidationMessage.PASSWORD_IS_INVALID,
+      'string.empty': UserValidationMessage.PASSWORD_REQUIRE,
+    }),
+  firstName: joi
+    .string()
+    .pattern(UserValidationRules.FIRST_NAME_REGEX)
+    .messages({
+      'string.pattern.base': UserValidationMessage.FIRST_NAME_IS_INVALID,
+      'string.empty': UserValidationMessage.FIRST_NAME_REQUIRE,
+    }),
+  lastName: joi.string().pattern(UserValidationRules.LAST_NAME_REGEX).messages({
     'string.pattern.base': UserValidationMessage.LAST_NAME_IS_INVALID,
     'string.empty': UserValidationMessage.LAST_NAME_REQUIRE,
   }),
