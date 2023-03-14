528c757f4a817c9bbbab950f4622725a86a38298
jaroslawtt <yaroslavlogan61907@gmail.com>
2023-03-14 22:14:08 +0200

    sl-16: * updated with prettify

--- frontend/src/libs/components/header/header.tsx
+++ frontend/src/libs/components/header/header.tsx
@@ -4,15 +4,17 @@ import styles from './header.module.scss';
 
 type Properties<T> = T;
 
-const Header: React.FC<Properties<React.PropsWithChildren>> = ({ children }) => {
+const Header: React.FC<Properties<React.PropsWithChildren>> = ({
+  children,
+}) => {
   return (
-     <header className={styles.header}>
-       <div className={styles.logoContainer}>
-         <div className={styles.headerLogo}/>
-         <span className={styles.headerLogoText}>logo</span>
-       </div>
-       <div className={styles.childrenContainer}>{ children }</div>
-     </header>
+    <header className={styles.header}>
+      <div className={styles.logoContainer}>
+        <div className={styles.headerLogo} />
+        <span className={styles.headerLogoText}>logo</span>
+      </div>
+      <div className={styles.childrenContainer}>{children}</div>
+    </header>
   );
 };
 
