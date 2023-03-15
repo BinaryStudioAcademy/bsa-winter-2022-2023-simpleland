32de4d5ccb6c3b22ebdd4f1649ac5b3bbbc9fc95
Mariia <ma.gaiduchenko@gmail.com>
2023-03-15 17:29:18 +0200

    sl-7: + first name and last name

--- backend/src/packages/users/user.entity.ts
+++ backend/src/packages/users/user.entity.ts
@@ -1,29 +1,39 @@
 import { type IEntity } from '~/libs/interfaces/interfaces.js';
 
 class UserEntity implements IEntity {
-  protected 'id': number | null;
+  private 'id': number | null;
 
-  protected 'email': string;
+  private 'email': string;
 
-  protected 'passwordHash': string;
+  private 'passwordHash': string;
 
-  protected 'passwordSalt': string;
+  private 'passwordSalt': string;
 
-  protected constructor({
+  private 'firstName': string;
+
+  private 'lastName': string;
+
+  private constructor({
     id,
     email,
     passwordHash,
     passwordSalt,
+    firstName,
+    lastName,
   }: {
     id: number | null;
     email: string;
     passwordHash: string;
     passwordSalt: string;
+    firstName: string;
+    lastName: string;
   }) {
     this.id = id;
     this.email = email;
-    this.passwordHash = passwordHash;
     this.passwordSalt = passwordSalt;
+    this.passwordHash = passwordHash;
+    this.firstName = firstName;
+    this.lastName = lastName;
   }
 
   public static initialize({
@@ -31,17 +41,23 @@ class UserEntity implements IEntity {
     email,
     passwordHash,
     passwordSalt,
+    firstName,
+    lastName,
   }: {
     id: number;
     email: string;
     passwordHash: string;
     passwordSalt: string;
+    firstName: string;
+    lastName: string;
   }): UserEntity {
     return new UserEntity({
       id,
       email,
       passwordHash,
       passwordSalt,
+      firstName,
+      lastName,
     });
   }
 
@@ -49,26 +65,36 @@ class UserEntity implements IEntity {
     email,
     passwordHash,
     passwordSalt,
+    firstName,
+    lastName,
   }: {
     email: string;
     passwordHash: string;
     passwordSalt: string;
+    firstName: string;
+    lastName: string;
   }): UserEntity {
     return new UserEntity({
       id: null,
       email,
       passwordHash,
       passwordSalt,
+      firstName,
+      lastName,
     });
   }
 
   public toObject(): {
     id: number;
     email: string;
+    firstName: string;
+    lastName: string;
   } {
     return {
       id: this.id as number,
       email: this.email,
+      firstName: this.firstName,
+      lastName: this.lastName,
     };
   }
 
@@ -76,11 +102,15 @@ class UserEntity implements IEntity {
     email: string;
     passwordHash: string;
     passwordSalt: string;
+    firstName: string;
+    lastName: string;
   } {
     return {
       email: this.email,
       passwordHash: this.passwordHash,
       passwordSalt: this.passwordSalt,
+      firstName: this.firstName,
+      lastName: this.lastName,
     };
   }
 }
