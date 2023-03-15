783d8b2f02f924067920634936f6c9ff09ecedb3
Mariia <ma.gaiduchenko@gmail.com>
2023-03-15 14:48:52 +0200

    sl-7: + relation mappings

--- backend/src/packages/users/user.model.ts
+++ backend/src/packages/users/user.model.ts
@@ -1,7 +1,11 @@
+import { Model } from 'objection';
+import { type RelationMappings } from 'objection';
+
 import {
   AbstractModel,
   DatabaseTableName,
 } from '~/libs/packages/database/database.js';
+import { UserDetailsModel } from '~/packages/users/user-details.model.js';
 
 class UserModel extends AbstractModel {
   public 'email': string;
@@ -10,8 +14,22 @@ class UserModel extends AbstractModel {
 
   public 'passwordSalt': string;
 
+  public userDetails: UserDetailsModel | undefined;
+
   public static override get tableName(): string {
     return DatabaseTableName.USERS;
+  }
+  public static override get relationMappings(): RelationMappings {
+    return {
+      userDetails: {
+        relation: Model.HasOneRelation,
+        modelClass: UserDetailsModel,
+        join: {
+          from: 'users.id',
+          to: 'user_details.user_id',
+        },
+      },
+    };
   }
 }
 
