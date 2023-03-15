03b8b42ac4ce6398ff41432d3eb6b6b9db46474f
Mariia <ma.gaiduchenko@gmail.com>
2023-03-15 14:36:10 +0200

    sl-7: * find method

--- backend/src/libs/interfaces/repository.interface.ts
+++ backend/src/libs/interfaces/repository.interface.ts
@@ -1,5 +1,5 @@
 interface IRepository<T = unknown> {
-  find(): Promise<T>;
+  find(id: unknown): Promise<T>;
   findAll(): Promise<T[]>;
   create(payload: unknown): Promise<T>;
   update(): Promise<T>;
