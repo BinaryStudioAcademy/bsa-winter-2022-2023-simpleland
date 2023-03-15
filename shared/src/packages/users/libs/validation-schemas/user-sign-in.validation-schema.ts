70b96243e919f464488494484e2c3c2b83d25651
Alyona Pisotska <pisotska.o@gmail.com>
2023-03-15 09:46:35 +0200

    sl-5: * import path for UserValidationRule

--- shared/src/packages/users/libs/validation-schemas/user-sign-in.validation-schema.ts
+++ shared/src/packages/users/libs/validation-schemas/user-sign-in.validation-schema.ts
@@ -1,8 +1,6 @@
 import joi from 'joi';
 
-import { UserValidationRule } from '~/packages/users/libs/enums/user-validation-rule.enum';
-
-import { UserValidationMessage } from '../enums/enums.js';
+import { UserValidationMessage, UserValidationRule } from '../enums/enums.js';
 import { type UserSignInRequestDto } from '../types/types.js';
 
 const userSignIn = joi.object<UserSignInRequestDto, true>({
