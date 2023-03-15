b97100f29dba32b2dc3359f9681e7587d8ff9e1b
AndriiSokyrko <sokyrko.andrii@gmail.com>
2023-03-15 14:10:23 +0200

    sl-14: + add cheking from repositiry

--- frontend/src/libs/components/protected-route/protected-route.tsx
+++ frontend/src/libs/components/protected-route/protected-route.tsx
@@ -1,17 +1,22 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import { Outlet } from 'react-router-dom';
 
 import { AppRoute } from '~/libs/enums/app-route.enum';
 import { useEffect, useNavigate } from '~/libs/hooks/hooks.js';
 import { storage, StorageKey } from '~/libs/packages/storage/storage.js';
+import { type UserGetAllItemResponseDto } from '~/packages/users/users.js';
 
 const ProtectedRoute = (): React.ReactElement => {
   const navigate = useNavigate();
+  const { users }  = useSelector((user: { users: UserGetAllItemResponseDto[], dataStatus: string }) => user);
+  const hasUser = Boolean(users);
+
   useEffect(() => {
     storage
       .get(StorageKey.TOKEN)
       .then((token) => {
-        if (!token) {
+        if (!token && !hasUser) {
           navigate(AppRoute.SIGN_IN);
         }
       })
@@ -19,7 +24,7 @@ const ProtectedRoute = (): React.ReactElement => {
         const typedError = error as Error;
         throw new Error(typedError.message);
       });
-  }, [navigate]);
+  }, [navigate, hasUser]);
 
   return <Outlet />;
 };
