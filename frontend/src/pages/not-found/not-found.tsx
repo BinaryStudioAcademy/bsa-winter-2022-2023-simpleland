edf878f0be14d9cc6eed1ae55223eaa1062133a8
Alexandr-mas <sashamaslov591@gmail.com>
2023-03-15 12:45:06 +0200

    sl-15: + delete unuse hook, refactor code

--- frontend/src/pages/not-found/not-found.tsx
+++ frontend/src/pages/not-found/not-found.tsx
@@ -1,13 +1,10 @@
 import styles from './not-found.module.scss';
 
 const ErrorPage = (): JSX.Element => {
-
   return (
     <div className={styles.errorPage}>
       <h1 className={styles.warningMessage}>Oops!</h1>
-      <p className={styles.text404}>
-        404
-      </p>
+      <p className={styles.text404}>404</p>
       <p>
         <i className={styles.status}>Page not found</i>
       </p>
