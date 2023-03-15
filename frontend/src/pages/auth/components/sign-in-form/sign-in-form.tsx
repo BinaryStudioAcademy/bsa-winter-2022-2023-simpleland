0915b734f5556f95c2122ea30ee2aae7b0c4763d
Vladyslav <billymosh@gmail.com>
2023-03-15 14:09:44 +0200

    sl-8: - deleted sample for select

--- frontend/src/pages/auth/components/sign-in-form/sign-in-form.tsx
+++ frontend/src/pages/auth/components/sign-in-form/sign-in-form.tsx
@@ -1,37 +1,7 @@
-import { useCallback,useState } from 'react';
-
-import { Button,Select } from '~/libs/components/components.js';
-import { type Option } from '~/libs/types/types.js';
+import { Button } from '~/libs/components/components.js';
 
 type Properties = {
   onSubmit: () => void;
-};
-
-const SelectSeeSample: React.FC = () => {
-
-  const options = [
-    { value: 'chocolate', label: 'Chocolate' },
-    { value: 'strawberry', label: 'Strawberry' },
-    { value: 'vanilla', label: 'Vanilla' },
-  ];
-  
-  const [selectedOption, setSelectedOption] = useState<Option | null>(null);
-
-  const handleChange = useCallback((option: Option | null) => {
-    setSelectedOption(option);
-  }, []);
-
-  return (
-    <div>
-    <h1>Select Example</h1>
-    <Select
-      options={options}
-      value={selectedOption}
-      onChange={handleChange}
-    />
-    </div>
-  );
-
 };
 
 const SignInForm: React.FC<Properties> = () => (
@@ -39,7 +9,6 @@ const SignInForm: React.FC<Properties> = () => (
     <h1>Sign In</h1>
     <form>
       <Button label="Sign in" />
-      <SelectSeeSample />
     </form>
   </>
 );
