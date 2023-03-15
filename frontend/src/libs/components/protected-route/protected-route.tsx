d3bc4e6e71b580136a43c4b6ba2ae6cb90155c13
AndriiSokyrko <sokyrko.andrii@gmail.com>
2023-03-15 16:13:55 +0200

    sl-14: * fix after review

--- frontend/src/libs/components/protected-route/protected-route.tsx
+++ frontend/src/libs/components/protected-route/protected-route.tsx
@@ -1,32 +1,17 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
 import { Outlet } from 'react-router-dom';
 
 import { AppRoute } from '~/libs/enums/app-route.enum';
-import { useEffect, useNavigate } from '~/libs/hooks/hooks.js';
-import { storage, StorageKey } from '~/libs/packages/storage/storage.js';
-import { type UserGetAllItemResponseDto } from '~/packages/users/users.js';
+import { useAppSelector,useNavigate } from '~/libs/hooks/hooks.js';
 
 const ProtectedRoute = (): React.ReactElement => {
   const navigate = useNavigate();
-  const { users } = useSelector(
-    (user: { users: UserGetAllItemResponseDto[]; dataStatus: string }) => user,
-  );
+  const { users } = useAppSelector((user) => user);
   const hasUser = Boolean(users);
 
-  useEffect(() => {
-    storage
-      .get(StorageKey.TOKEN)
-      .then((token) => {
-        if (!token && !hasUser) {
-          navigate(AppRoute.SIGN_IN);
-        }
-      })
-      .catch((error) => {
-        const typedError = error as Error;
-        throw new Error(typedError.message);
-      });
-  }, [navigate, hasUser]);
+  if (!hasUser) {
+    navigate(AppRoute.SIGN_IN);
+  }
 
   return <Outlet />;
 };
