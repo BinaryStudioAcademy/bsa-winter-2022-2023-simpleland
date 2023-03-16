e3e07afc7cb9eaed112a6eddcbd76af43c6ebae7
Vladyslav <billymosh@gmail.com>
2023-03-16 21:56:05 +0200

    sl-8: * changes classes from scss to select api(without icon/dropdown not work correctly)

--- frontend/src/libs/components/select/select.tsx
+++ frontend/src/libs/components/select/select.tsx
@@ -4,13 +4,11 @@ import {
   type FieldPath,
   type FieldValues,
 } from 'react-hook-form';
-import { type Theme } from 'react-select';
-import Select from 'react-select';
+import Select, { type StylesConfig } from 'react-select';
 
 import { useFormController } from '~/libs/hooks/hooks.js';
 
 import { type Options } from '../../types/types.js';
-import styles from './styles.module.scss';
 
 type SelectProperties<T extends FieldValues> = {
   control: Control<T, null>;
@@ -24,15 +22,49 @@ type SelectProperties<T extends FieldValues> = {
   errors: FieldErrors<T>;
 };
 
-const SelectTheme = (theme: Theme): Theme => ({
-  ...theme,
-  borderRadius: 0,
-  colors: {
-    ...theme.colors,
-    primary25: 'white',
-    primary: 'white',
+const SelectStyles: StylesConfig<Options> = {
+  control: (provided) => ({
+    ...provided,
+    border: '2px solid #FFB61D',
+    '&:hover': {
+      border: '2px solid #FFB61D',
+    },
+    width: '324px',
+    cursor: 'pointer',
+
+  }),
+  option: (provided, state) => ({
+    ...provided,
+    color: '#343434',
+    backgroundColor: state.isSelected ? '#FFB61D' : 'white',
+    '&:hover': {
+      backgroundColor: '#C4C4C4',
+    },
+    cursor: 'pointer',
+  }),
+  menu: (provided) => ({
+    ...provided,
+    backgroundColor: 'white',
+    borderRadius: '0',
+    boxShadow: '0px 4px 4px rgba(0, 0, 0, 0.1)',
+    marginTop: '0',
+    cursor: 'pointer',
+    width: '324px',
+  }),
+  placeholder: (provided) => ({
+    ...provided,
+    color: '#C9C9C9',
+  }),
+  dropdownIndicator: (provided, state) => {
+    const isFocused = state.isFocused;
+    const rotation = isFocused ? '180deg' : '0deg';
+    return {
+      ...provided,
+      color: '#343434',
+      transform: `rotate(${rotation})`,
+    };
   },
-});
+};
 
 const SelectComponent = <T extends FieldValues>({
   control,
@@ -48,19 +80,18 @@ const SelectComponent = <T extends FieldValues>({
   const hasError = Boolean(error);
 
   return (
-    <div className={styles.wrapper}>
-      <label className={styles.label}>{label}</label>
-      <div className={styles.selectWrapper}>
+    <div>
+      <label>{label}</label>
+      <div>
         <Select
           {...field}
           value={options.find((option) => option.value === field.value)}
-          className={styles.select}
           defaultValue={null}
           classNamePrefix="react-select"
           options={options}
           placeholder={placeholder}
-          theme={SelectTheme}
           name={name}
+          styles={SelectStyles}
         />
       </div>
       {hasError && <span>{error as string}</span>}
