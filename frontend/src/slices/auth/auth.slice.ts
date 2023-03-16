796c8c07dac4dcc637be5d2f9a8f0f809ab378af
vbazhynov <vladbazhynov@gmail.com>
2023-03-16 12:51:15 +0200

    sl-9: + add token to local storage

--- frontend/src/slices/auth/auth.slice.ts
+++ frontend/src/slices/auth/auth.slice.ts
@@ -1,16 +1,20 @@
 import { createSlice } from '@reduxjs/toolkit';
+import { type UserSignUpResponseDto } from 'shared/build/index.js';
 
 import { DataStatus } from '~/libs/enums/enums.js';
+import { storage, StorageKey } from '~/libs/packages/storage/storage.js';
 import { type ValueOf } from '~/libs/types/types.js';
 
 import { signUp } from './actions.js';
 
 type State = {
   dataStatus: ValueOf<typeof DataStatus>;
+  user: UserSignUpResponseDto;
 };
 
 const initialState: State = {
   dataStatus: DataStatus.IDLE,
+  user: { token: '', user: { id: 1, email: '' } },
 };
 
 const { reducer, actions, name } = createSlice({
@@ -21,8 +25,10 @@ const { reducer, actions, name } = createSlice({
     builder.addCase(signUp.pending, (state) => {
       state.dataStatus = DataStatus.PENDING;
     });
-    builder.addCase(signUp.fulfilled, (state) => {
+    builder.addCase(signUp.fulfilled, (state, action) => {
       state.dataStatus = DataStatus.FULFILLED;
+      void storage.set(StorageKey.TOKEN, action.payload.token);
+      state.user = action.payload;
     });
     builder.addCase(signUp.rejected, (state) => {
       state.dataStatus = DataStatus.REJECTED;
