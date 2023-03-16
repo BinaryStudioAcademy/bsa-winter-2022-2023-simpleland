e5593ce4b7a99f62dd98e5479af3d0f077ffe41b
AndriiSokyrko <sokyrko.andrii@gmail.com>
2023-03-16 20:39:38 +0200

    sl-14: * fix change structure of auth slice

--- frontend/src/libs/components/protected-route/protected-route.tsx
+++ frontend/src/libs/components/protected-route/protected-route.tsx
@@ -1,18 +1,15 @@
-import { AppRoute, DataStatus } from '~/libs/enums/enums.js';
-import { useAppSelector, useEffect, useNavigate } from '~/libs/hooks/hooks.js';
+import { AppRoute } from '~/libs/enums/enums.js';
+import { Navigate, useAppSelector } from '~/libs/hooks/hooks.js';
 
 type Properties = {
   children: React.ReactNode;
 };
 const ProtectedRoute: React.FC<Properties> = ({ children }: Properties) => {
-  const { auth } = useAppSelector((auth) => auth);
-  const navigate = useNavigate();
-
-  useEffect(() => {
-    if (auth.dataStatus === DataStatus.IDLE) {
-      navigate(AppRoute.SIGN_IN);
-    }
-  }, [auth, navigate]);
+  const { user } = useAppSelector(({ auth }) => ({ user: auth.user }));
+  const hasAuthenticatedUser = Boolean(user);
+  if (!hasAuthenticatedUser) {
+    return <Navigate to={AppRoute.SIGN_IN} replace />;
+  }
 
   return <>{children}</>;
 };
