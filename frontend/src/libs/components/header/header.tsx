49a472ee46de6f342858fbcbebefc521a1b77ecd
jaroslawtt <yaroslavlogan61907@gmail.com>
2023-03-16 09:24:48 +0200

    sl-16: * updated header component and styles

--- frontend/src/libs/components/header/header.tsx
+++ frontend/src/libs/components/header/header.tsx
@@ -1,19 +1,33 @@
-import React from 'react';
-
 import styles from './header.module.scss';
 
-type Properties<T> = T;
+type Properties = {
+  caption: string;
+  username: string;
+  image: string;
+};
 
-const Header: React.FC<Properties<React.PropsWithChildren>> = ({
-  children,
-}) => {
+const Header: React.FC<Properties> = ({
+  caption,
+  username,
+  image,
+}: Properties) => {
   return (
     <header className={styles.header}>
       <div className={styles.logoContainer}>
         <div className={styles.headerLogo} />
         <span className={styles.headerLogoText}>logo</span>
       </div>
-      <div className={styles.childrenContainer}>{children}</div>
+      <div className={styles.headerSidebar}>
+        <span className={styles.headerSectionTitle}>{caption}</span>
+        <div className={styles.headerProfileData}>
+          <img
+            src={image}
+            alt="profile-icon"
+            className={styles.headerProfileIcon}
+          />
+          <span className={styles.headerProfileCaption}>{username}</span>
+        </div>
+      </div>
     </header>
   );
 };
