38a677128c474edda0cc5dcc34f776ce75a35ec5
Mariia <ma.gaiduchenko@gmail.com>
2023-03-17 13:56:31 +0200

    sl-19: + projects dtos

---
+++ shared/src/packages/projects/libs/types/types.ts
@@ -0,0 +1,4 @@
+export { type ProjectCreateRequestDto } from './project-create-request-dto.type.js';
+export { type ProjectCreateResponseDto } from './project-create-response-dto.type.js';
+export { type ProjectGetAllItemResponseDto } from './project-get-all-item-response-dto.type.js';
+export { type ProjectGetAllResponseDto } from './project-get-all-response-dto.type.js';
