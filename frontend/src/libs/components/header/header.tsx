ce0a0c8729c1b6f75aa5a8b7747e47d408f2ab62
jaroslawtt <yaroslavlogan61907@gmail.com>
2023-03-18 19:28:02 +0200

    sl-16: * updated componet's jsx with prettify

--- frontend/src/libs/components/header/header.tsx
+++ frontend/src/libs/components/header/header.tsx
@@ -22,13 +22,17 @@ const Header: React.FC<Properties> = ({ user: authUserData }: Properties) => {
       <div className={styles.headerSidebar}>
         <ul className={styles.headerNavList}>
           <li>
-            <Link to='/'>
+            <Link to="/">
               <span className={styles.sectionTitle}>My sites</span>
             </Link>
           </li>
         </ul>
         <div className={styles.profileDataWrapper}>
-          <img alt='profile' src={profileImage} className={styles.profileIcon} />
+          <img
+            alt="profile"
+            src={profileImage}
+            className={styles.profileIcon}
+          />
           <span className={styles.profileCaption}>
             {user?.firstName} {user?.lastName}
           </span>
