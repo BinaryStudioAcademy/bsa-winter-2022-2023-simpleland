6e45971d663a835a09a98236949974c53d8508e7
jaroslawtt <yaroslavlogan61907@gmail.com>
2023-03-18 21:48:01 +0200

    sl-16: * added required fields to sign in response dto, removed redudant code

--- frontend/src/libs/components/header/header.tsx
+++ frontend/src/libs/components/header/header.tsx
@@ -1,6 +1,5 @@
 import profileImage from '~/assets/img/profile-image.png';
 import { Link } from '~/libs/components/components.js';
-import { useAppSelector } from '~/libs/hooks/hooks.js';
 import { type UserSignInResponseDto } from '~/packages/users/users.js';
 
 import styles from './styles.module.scss';
@@ -9,10 +8,7 @@ type Properties = {
   user: UserSignInResponseDto | null;
 };
 
-const Header: React.FC<Properties> = ({ user: authUserData }: Properties) => {
-  const { users } = useAppSelector(({ users }) => users);
-  const user = users.find((user) => user.id === authUserData?.id);
-
+const Header: React.FC<Properties> = ({ user }: Properties) => {
   return (
     <header className={styles.header}>
       <div className={styles.logoWrapper}>
--- shared/src/packages/users/libs/types/user-sign-in-response-dto.type.ts
+++ shared/src/packages/users/libs/types/user-sign-in-response-dto.type.ts
@@ -1,6 +1,8 @@
 type UserSignInResponseDto = {
   id: number;
   email: string;
+  firstName: string;
+  lastName: string;
 };
 
 export { type UserSignInResponseDto };
