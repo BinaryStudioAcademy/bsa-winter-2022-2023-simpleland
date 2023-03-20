7042ecb0a078a2bc9b203f54d15507ac582d3fde
jaroslawtt <yaroslavlogan61907@gmail.com>
2023-03-20 22:08:32 +0200

    sl-51: + wrapped modal with portal component, added backdrop element and styles for it

--- frontend/src/libs/components/modal-window/modal-window.tsx
+++ frontend/src/libs/components/modal-window/modal-window.tsx
@@ -1,14 +1,19 @@
+import { Portal } from 'react-portal';
+
 import closePopUpIcon from '~/assets/img/close-pop-up-icon.svg';
 
 import styles from './styles.module.scss';
 
 const ModalWindow: React.FC = () => {
   return (
-    <div className={styles.modal}>
-      <button className={styles.modalPopUpClose}>
-        <img alt="close" src={closePopUpIcon} />
-      </button>
-    </div>
+    <Portal>
+      <div className={styles.backdrop} />
+      <div className={styles.modal}>
+        <button className={styles.modalPopUpClose}>
+          <img alt="close" src={closePopUpIcon} />
+        </button>
+      </div>
+    </Portal>
   );
 };
 
