c3b601fdd92a6116ef8194e9b93cd8594030fb64
jaroslawtt <yaroslavlogan61907@gmail.com>
2023-03-21 14:20:18 +0200

    sl-51: + added state isOpen and callback for closing modal

--- frontend/src/libs/components/modal/modal.tsx
+++ frontend/src/libs/components/modal/modal.tsx
@@ -1,27 +1,41 @@
+import { useCallback } from 'react';
 import { createPortal } from 'react-dom';
 
 import { IconButton } from '~/libs/components/components.js';
+import { useState } from '~/libs/hooks/hooks.js';
 
 import styles from './styles.module.scss';
 
 type Properties = {
-  children: React.ReactNode;
+  children?: React.ReactNode;
 };
 
-const Modal: React.FC<Properties> = ({ children }) => {
-  return createPortal(
-    <dialog className={styles.modal}>
-      <div className={styles.modalInner}>
-        <IconButton
-          icon="cross"
-          label="Close modal"
-          className={styles.modalPopUpClose as string}
-        />
-        {children}
-      </div>
-      <div className={styles.backdrop} />
-    </dialog>,
-    document.body,
+const Modal: React.FC<Properties> = ({ children }: Properties) => {
+  const [isOpen, setIsOpen] = useState(true);
+
+  const onClose = useCallback(() => setIsOpen(false), [setIsOpen]);
+
+  return (
+    <>
+      {isOpen &&
+        createPortal(
+          <>
+            <dialog className={styles.modal} id="modal">
+              <div className={styles.modalInner}>
+                <IconButton
+                  icon="cross"
+                  label="Close modal"
+                  className={styles.modalPopUpClose as string}
+                  onClick={onClose}
+                />
+                {children}
+              </div>
+              <div className={styles.backdrop} />
+            </dialog>
+          </>,
+          document.body,
+        )}
+    </>
   );
 };
 
