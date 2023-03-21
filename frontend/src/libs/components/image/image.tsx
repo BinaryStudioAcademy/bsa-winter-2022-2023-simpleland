184bd323f92cbcffc27b3db124a2eef786208398
vbazhynov <vladbazhynov@gmail.com>
2023-03-21 19:13:44 +0200

    sl-43: + add image component

---
+++ frontend/src/libs/components/image/image.tsx
@@ -0,0 +1,29 @@
+import { getValidClassNames } from '~/libs/helpers/helpers.js';
+
+import styles from './styles.module.scss';
+
+type Properties = {
+  src: string;
+  alt: string;
+  className?: string;
+  width?: string;
+  height?: string;
+};
+
+const Image: React.FC<Properties> = ({
+  src,
+  alt,
+  className = '',
+  width,
+  height,
+}: Properties) => (
+  <img
+    className={getValidClassNames(styles.image, className)}
+    width={width}
+    height={height}
+    src={src}
+    alt={alt}
+  />
+);
+
+export { Image };
