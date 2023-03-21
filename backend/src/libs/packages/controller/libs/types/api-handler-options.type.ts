4977732ab33de7e30fc9043e7ef9c755509661c0
Mariia <ma.gaiduchenko@gmail.com>
2023-03-21 17:14:19 +0200

    sl-11: - headers type

--- backend/src/libs/packages/controller/libs/types/api-handler-options.type.ts
+++ backend/src/libs/packages/controller/libs/types/api-handler-options.type.ts
@@ -1,23 +1,20 @@
-import { type FastifyRequest } from 'fastify';
-
+import { type ServerAppRouteParameters } from '~/libs/packages/server-application/server-application.js';
 import { type UserAuthResponse } from '~/packages/users/users.js';
 
 type DefaultApiHandlerOptions = {
+  headers?: Parameters<ServerAppRouteParameters['handler']>[0]['headers'];
   body?: unknown;
   query?: unknown;
   params?: unknown;
 };
 
-type HeadersInjected = {
-  headers: FastifyRequest['headers'];
-};
-
 type ApiHandlerOptions<
   T extends DefaultApiHandlerOptions = DefaultApiHandlerOptions,
-> = HeadersInjected & {
+> = {
   body: T['body'];
   query: T['query'];
   params: T['params'];
+  headers: Parameters<ServerAppRouteParameters['handler']>[0]['headers'];
   user: UserAuthResponse | null;
 };
 
