b4eb0de15068efca514fe4d0a2ddc8ad69dadaf3
jaroslawtt <yaroslavlogan61907@gmail.com>
2023-03-21 09:36:08 +0200

    sl-51: * replaced using Portal component with ReactDOM create portal

--- frontend/src/libs/components/modal-window/modal-window.tsx
+++ frontend/src/libs/components/modal-window/modal-window.tsx
@@ -1,19 +1,20 @@
-import { Portal } from 'react-portal';
+import ReactDOM from 'react-dom';
 
 import closePopUpIcon from '~/assets/img/close-pop-up-icon.svg';
 
 import styles from './styles.module.scss';
 
 const ModalWindow: React.FC = () => {
-  return (
-    <Portal>
+  return ReactDOM.createPortal(
+    <>
       <div className={styles.backdrop} />
       <div className={styles.modal}>
         <button className={styles.modalPopUpClose}>
           <img alt="close" src={closePopUpIcon} />
         </button>
       </div>
-    </Portal>
+    </>,
+    document.body,
   );
 };
 
