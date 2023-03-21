c8a597bfea5fd0bfd7f4c0cf2bbef545cdce9f0f
jaroslawtt <yaroslavlogan61907@gmail.com>
2023-03-21 13:49:05 +0200

    sl-51: * used dialog element to create modal, updated styles due to dialog requirements

--- frontend/src/libs/components/modal/modal.tsx
+++ frontend/src/libs/components/modal/modal.tsx
@@ -4,18 +4,23 @@ import { IconButton } from '~/libs/components/components.js';
 
 import styles from './styles.module.scss';
 
-const Modal: React.FC = () => {
+type Properties = {
+  children: React.ReactNode;
+};
+
+const Modal: React.FC<Properties> = ({ children }) => {
   return createPortal(
-    <>
-      <div className={styles.backdrop} />
-      <div className={styles.modal}>
+    <dialog className={styles.modal}>
+      <div className={styles.modalInner}>
         <IconButton
           icon="cross"
           label="Close modal"
           className={styles.modalPopUpClose as string}
         />
+        {children}
       </div>
-    </>,
+      <div className={styles.backdrop} />
+    </dialog>,
     document.body,
   );
 };
