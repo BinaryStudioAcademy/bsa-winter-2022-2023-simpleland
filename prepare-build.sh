da1f572f3ea10fe0246ca5205522b64c36dac6d9
andanf-e <andanfe@gmail.com>
2023-03-22 23:07:16 +0200

    sl-3: * fix prepare build script

--- prepare-build.sh
+++ prepare-build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-mkdir -p ./build/backend/; cp ./backend/build/* ./build/backend/; rm -rf ./backend/build/;
-mkdir ./build/shared/; cp ./shared/build ./build/shared/
-mkdir ./build/backend/public; cp ./frontend/build/* ./build/backend/public; rm -rf ./frontend/build
+mkdir -p ./build/backend/; mv ./backend/build/* ./build/backend/;
+mkdir ./build/shared/; mv ./shared/build ./build/shared/
+mkdir ./build/backend/public; mv ./frontend/build/* ./build/backend/public;
 cp package.json package-lock.json ./build
