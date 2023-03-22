eba2cdb1e0733202a0900ee0e12268b79b95a894
vbazhynov <vladbazhynov@gmail.com>
2023-03-22 09:19:16 +0200

    sl-43: * add type for find by userId request

--- backend/src/packages/projects/libs/types/types.ts
+++ backend/src/packages/projects/libs/types/types.ts
@@ -1,5 +1,7 @@
+type FindProjectByUserIdRequest = { userId: string };
 export {
   type ProjectCreateRequestDto,
   type ProjectCreateResponseDto,
   type ProjectGetAllResponseDto,
 } from 'shared/build/index.js';
+export { type FindProjectByUserIdRequest };
