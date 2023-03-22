6560509d12f63c13ca83df3f35d8886f7eae7757
andanf-e <andanfe@gmail.com>
2023-03-22 23:42:41 +0200

    sl-3: + build in workspaces test

--- prepare-build.sh
+++ prepare-build.sh
@@ -3,6 +3,6 @@
 mkdir -p ./build/backend/; mv ./backend/build/* ./build/backend/;
 mkdir ./build/shared/; mv ./shared/build ./build/shared/
 mkdir ./build/backend/public; mv ./frontend/build/* ./build/backend/public;
-mkdir ./build/node_modules; mv ./node_modules/* ./build/node_modules;
+# mkdir ./build/node_modules; mv ./node_modules/* ./build/node_modules;
 cp package.json package-lock.json ./build;
 
