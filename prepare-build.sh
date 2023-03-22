9cfd30ad3ba8e48087e57ab288d22334416bb028
andanf-e <andanfe@gmail.com>
2023-03-22 23:18:06 +0200

    sl-3: * make node_modules dir in prepare script

--- prepare-build.sh
+++ prepare-build.sh
@@ -3,5 +3,6 @@
 mkdir -p ./build/backend/; mv ./backend/build/* ./build/backend/;
 mkdir ./build/shared/; mv ./shared/build ./build/shared/
 mkdir ./build/backend/public; mv ./frontend/build/* ./build/backend/public;
+mkdir ./build/node_modules; mv ./node_modules/* ./build/node_modules;
 cp package.json package-lock.json ./build;
-cp ./node_modules/* ./build/node_modules;
+
