460536fc869fa6f68c5248efea8714e328c1a999
andanf-e <andanfe@gmail.com>
2023-03-23 01:02:55 +0200

    sl-3: - unused shared build mv

--- prepare-build.sh
+++ prepare-build.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
 mkdir -p ./build/backend/; mv ./backend/build/* ./build/backend/;
-mkdir ./build/shared/; mv ./shared/build ./build/shared/
 mkdir ./build/backend/public; mv ./frontend/build/* ./build/backend/public;
-# mkdir ./build/node_modules; mv ./node_modules/* ./build/node_modules;
 cp package.json package-lock.json ./build;
 
