a7a97fc361ba4d92f94c343fb4e75fe984761276
andanf-e <andanfe@gmail.com>
2023-03-22 23:03:34 +0200

    sl-3: * fix start build script

--- prepare-build.sh
+++ prepare-build.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-# mb need to add copying node_modules to build folder (or backend/build) :: cp ./node_modules ./build
 mkdir -p ./build/backend/; cp ./backend/build/* ./build/backend/; rm -rf ./backend/build/;
 mkdir ./build/shared/; cp ./shared/build ./build/shared/
 mkdir ./build/backend/public; cp ./frontend/build/* ./build/backend/public; rm -rf ./frontend/build
