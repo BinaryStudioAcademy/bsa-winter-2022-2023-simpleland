2886c1202316048fdd7c44c8d8feb6f07c6b0e9c
vbazhynov <vladbazhynov@gmail.com>
2023-03-22 09:14:38 +0200

    sl-43: * add api routes for projects

--- frontend/src/libs/enums/app-route.enum.ts
+++ frontend/src/libs/enums/app-route.enum.ts
@@ -2,6 +2,7 @@ const AppRoute = {
   ROOT: '/',
   SIGN_IN: '/sign-in',
   SIGN_UP: '/sign-up',
+  MY_PROJECTS: '/my-projects',
   ANY: '*',
 } as const;
 
--- shared/src/packages/projects/libs/enums/projects-api-path.enum.ts
+++ shared/src/packages/projects/libs/enums/projects-api-path.enum.ts
@@ -1,5 +1,6 @@
 const ProjectsApiPath = {
   ROOT: '/',
+  USER_ID: '/:userId',
 } as const;
 
 export { ProjectsApiPath };
