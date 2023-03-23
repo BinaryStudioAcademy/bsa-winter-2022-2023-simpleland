6ac1ad6d45f7a65f9aa942f6bac4ec88de8fd076
Dima Popovych <dimapopovych.w@gmail.com>
2023-03-23 11:27:08 +0200

    sl-60: + sections migrations

---
+++ backend/src/db/migrations/20230323090957_add_sections_table.ts
@@ -0,0 +1,33 @@
+import { type Knex } from 'knex';
+
+const TABLE_NAME = 'sections';
+
+const ColumnName = {
+  ID: 'id',
+  NAME: 'name',
+  CONTENT: 'content',
+  CREATED_AT: 'created_at',
+  UPDATED_AT: 'updated_at',
+};
+
+function up(knex: Knex): Promise<void> {
+  return knex.schema.createTable(TABLE_NAME, (table) => {
+    table.increments(ColumnName.ID).primary();
+    table.string(ColumnName.NAME).notNullable();
+    table.text(ColumnName.CONTENT).notNullable();
+    table
+      .dateTime(ColumnName.CREATED_AT)
+      .notNullable()
+      .defaultTo(knex.fn.now());
+    table
+      .dateTime(ColumnName.UPDATED_AT)
+      .notNullable()
+      .defaultTo(knex.fn.now());
+  });
+}
+
+function down(knex: Knex): Promise<void> {
+  return knex.schema.dropTableIfExists(TABLE_NAME);
+}
+
+export { down, up };
