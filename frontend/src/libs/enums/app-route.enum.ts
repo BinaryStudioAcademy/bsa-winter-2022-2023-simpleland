3a19fee2e35a29cfbddce4d1edfb71df7215c8e6
Alyona Pisotska <pisotska.o@gmail.com>
2023-03-23 13:15:10 +0200

    sl-62: + start page route

--- frontend/src/libs/enums/app-route.enum.ts
+++ frontend/src/libs/enums/app-route.enum.ts
@@ -2,6 +2,7 @@ const AppRoute = {
   ROOT: '/',
   SIGN_IN: '/sign-in',
   SIGN_UP: '/sign-up',
+  START: 'start',
   PROFILE: '/profile',
   ANY: '*',
 } as const;
---
+++ frontend/src/pages/start/start.tsx
@@ -0,0 +1,11 @@
+import { PageLayout } from '~/libs/components/components.js';
+
+const StartPage: React.FC = () => {
+  return (
+    <PageLayout style="black">
+      <div>Start page</div>
+    </PageLayout>
+  );
+};
+
+export { StartPage };
