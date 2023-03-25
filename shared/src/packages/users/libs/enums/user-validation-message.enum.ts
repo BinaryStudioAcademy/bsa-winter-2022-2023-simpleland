97e9fc885fec38eeb55d23170012a92e23c121c7
Alyona Pisotska <pisotska.o@gmail.com>
2023-03-25 09:26:34 +0200

    Revert "Revert "Merge branch 'development' of https://github.com/BinaryStudioAcademy/bsa-winter-2022-2023-simpleland into task/sl-52-sites-page""
    This reverts commit ed335dc121be6ebd3e5cd7d400ff58f468e7aa4c.

--- shared/src/packages/users/libs/enums/user-validation-message.enum.ts
+++ shared/src/packages/users/libs/enums/user-validation-message.enum.ts
@@ -1,14 +1,13 @@
 const UserValidationMessage = {
   EMAIL_REQUIRE: 'Email is required',
   EMAIL_WRONG: 'Email is wrong',
-  EMAIL_IS_INVALID: 'Invalid email format',
-  PASSWORD_IS_INVALID:
-    'Password must contain 8-30 characters and can contain only letters, number and special characters',
+  EMAIL_IS_INVALID: 'Incorrect email format',
+  PASSWORD_IS_INVALID: 'Incorrect password',
   PASSWORD_REQUIRE: 'Password is required',
   FIRST_NAME_REQUIRE: 'First name is required',
-  FIRST_NAME_IS_INVALID: 'Invalid first name',
+  FIRST_NAME_IS_INVALID: 'Incorrect first name',
   LAST_NAME_REQUIRE: 'Last name is required',
-  LAST_NAME_IS_INVALID: 'Invalid last name',
+  LAST_NAME_IS_INVALID: 'Incorrect last name',
 } as const;
 
 export { UserValidationMessage };
