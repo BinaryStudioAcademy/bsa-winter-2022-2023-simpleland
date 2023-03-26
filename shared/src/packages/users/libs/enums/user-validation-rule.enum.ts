5b4e953653df5fb93ed01f148bc97a538026ae5a
Mariia <ma.gaiduchenko@gmail.com>
2023-03-26 12:23:43 +0300

    sl-80: * user val rule email

--- shared/src/packages/users/libs/enums/user-validation-rule.enum.ts
+++ shared/src/packages/users/libs/enums/user-validation-rule.enum.ts
@@ -1,6 +1,6 @@
 const UserValidationRule = {
   EMAIL_REGEX:
-    /^(\w)([\w+.-]{0,33}\w)?@[\dA-Za-z][\dA-Za-z-]{0,61}[\dA-Za-z]\.[A-Za-z]{2,}$/,
+    /^(\w)([\w+.-]{0,33}\w)?@[\dA-Za-z][\dA-Za-z-]{0,66}[\dA-Za-z]\.[A-Za-z]{2,}$/,
   PASSWORD_REGEX:
     /^(?=.*[A-Z])(?=.*\d)(?=.*[!"#$%&'()*+,./:;<=>?@[\\\]^_`{|}~-])(?=.*[a-z]).{8,30}$/,
   FIRST_NAME_REGEX: /^[A-Z]['a-z-]{1,29}$/,
