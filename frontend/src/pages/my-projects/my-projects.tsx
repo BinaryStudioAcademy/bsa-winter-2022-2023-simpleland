b26038abdb8861830cbc81a623622753cc62cc0c
Mariia <ma.gaiduchenko@gmail.com>
2023-03-27 16:14:12 +0300

    sl-88: + searching projects feature

--- frontend/src/pages/my-projects/my-projects.tsx
+++ frontend/src/pages/my-projects/my-projects.tsx
@@ -1,9 +1,15 @@
-import { Button, PageLayout } from '~/libs/components/components.js';
+import { type FieldValues, type SubmitHandler } from 'react-hook-form';
+
+import { Button, Input, PageLayout } from '~/libs/components/components.js';
 import { AppRoute } from '~/libs/enums/enums.js';
 import {
   useAppDispatch,
   useAppSelector,
+  useCallback,
   useEffect,
+  useForm,
+  useMemo,
+  useState,
 } from '~/libs/hooks/hooks.js';
 import { actions as projectActions } from '~/slices/projects/projects.js';
 
@@ -21,16 +27,61 @@ const MyProjects: React.FC = () => {
     projects: state.projects.projects,
   }));
 
-  const hasProjects = projects.length > 0;
+  const [searchQuery, setSearchQuery] = useState<string>('');
+
+  const filteredProjects = useMemo(() => {
+    return projects.filter((project) =>
+      project.name.toLowerCase().includes(searchQuery.toLowerCase()),
+    );
+  }, [projects, searchQuery]);
+
+  const handleChange: SubmitHandler<FieldValues> = useCallback(
+    (data: FieldValues) => {
+      setSearchQuery(data['search'] as string);
+    },
+    [setSearchQuery],
+  );
+
+  const {
+    control,
+    formState: { errors },
+    handleSubmit,
+  } = useForm({
+    mode: 'onChange',
+  });
+
+  const handleFormChange = useCallback(
+    (event_: React.BaseSyntheticEvent): void => {
+      void handleSubmit((data) => handleChange(data))(event_);
+    },
+    [handleSubmit, handleChange],
+  );
+
+  const hasProjects = filteredProjects.length > 0;
 
   return (
     <PageLayout pageName="My Projects">
       {hasProjects ? (
-        <div className={styles['cards-wrapper']}>
-          {projects.map((card) => (
-            <ProjectCard key={card.id} project={card} />
-          ))}
-        </div>
+        <>
+          <div className={styles['wrapper']}>
+            <div className={styles['cards-wrapper']}>
+              {filteredProjects.map((card) => (
+                <ProjectCard key={card.id} project={card} />
+              ))}
+            </div>
+            <form onChange={handleFormChange}>
+              <div className={styles['search-input-wrapper']}>
+                <Input
+                  type="search"
+                  placeholder="Search"
+                  name="search"
+                  control={control}
+                  errors={errors}
+                />
+              </div>
+            </form>
+          </div>
+        </>
       ) : (
         <div className={styles['placeholder']}>
           <div className={styles['placeholder-caption']}>
