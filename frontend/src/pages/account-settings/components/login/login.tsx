8288aab80e8378b4c327ee6d79b9a147d5ff9a61
jaroslawtt <yaroslavlogan61907@gmail.com>
2023-03-28 22:50:25 +0300

    sl-104: + extended login component logic

--- frontend/src/pages/account-settings/components/login/login.tsx
+++ frontend/src/pages/account-settings/components/login/login.tsx
@@ -1,7 +1,69 @@
+import { type UserAuthResponse,type UserUpdateLoginRequestDto,userUpdateLoginValidationSchema } from 'shared/build/index.js';
+
+import { Button, Input } from '~/libs/components/components.js';
+import { getValidClassNames } from '~/libs/helpers/helpers.js';
+import { useAppForm } from '~/libs/hooks/hooks.js';
+
 import styles from './styles.module.scss';
 
-const Login: React.FC = () => {
-  return <form className={styles['form-wrapper']}>form</form>;
+type Properties = {
+  user: UserAuthResponse;
+};
+
+const Login: React.FC<Properties> = ({ user }: Properties) => {
+
+  const { control, errors, handleReset } =
+    useAppForm<UserUpdateLoginRequestDto>({
+      defaultValues: {
+        login: user.email,
+        password: user.accountName ?? '',
+        },
+      validationSchema: userUpdateLoginValidationSchema,
+    });
+
+  return <form className={styles['form-wrapper']}>
+    <div className={styles['inputs']}>
+      <Input
+        type="text"
+        label="E-mail"
+        placeholder="name@gmail.com"
+        name="login"
+        control={control}
+        errors={errors}
+      />
+      <Input
+        type="password"
+        label="Password"
+        placeholder="password"
+        name="password"
+        control={control}
+        errors={errors}
+      />
+    </div>
+    <div className={styles['captions']}>
+      <span className={styles['caption']}>Change Password</span>
+    </div>
+    <div className={styles['buttons']}>
+      <Button
+        type="button"
+        style="secondary"
+        size="small"
+        label="Cancel"
+        className={styles['button']}
+        onClick={handleReset}
+      />
+      <Button
+        type="submit"
+        style="primary"
+        size="small"
+        label="Save Changes"
+        className={getValidClassNames(
+                styles['button'],
+                styles['submit-button'],
+                )}
+      />
+    </div>
+  </form>;
 };
 
 export { Login };
