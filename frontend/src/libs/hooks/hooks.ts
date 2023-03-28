52c5e9eda8e8c138a55d2a0ac63d4b95f8931e81
Mariia <ma.gaiduchenko@gmail.com>
2023-03-28 23:17:39 +0300

    sl-88: + debounce hook

--- frontend/src/libs/hooks/hooks.ts
+++ frontend/src/libs/hooks/hooks.ts
@@ -1,3 +1,4 @@
+export { useAppDebounce } from './use-app-debounce/use-app-debounce.hook.js';
 export { useAppDispatch } from './use-app-dispatch/use-app-dispatch.hook.js';
 export { useAppForm } from './use-app-form/use-app-form.hook.js';
 export { useAppSelector } from './use-app-selector/use-app-selector.hook.js';
