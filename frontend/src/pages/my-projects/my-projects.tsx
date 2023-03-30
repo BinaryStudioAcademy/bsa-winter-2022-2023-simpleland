882e83eaeed2273577ca5dbb612984909b168039
Mariia <ma.gaiduchenko@gmail.com>
2023-03-30 18:20:14 +0300

    sl-88: * search functionality

--- frontend/src/pages/my-projects/my-projects.tsx
+++ frontend/src/pages/my-projects/my-projects.tsx
@@ -1,18 +1,18 @@
 import { Button, Input, PageLayout } from '~/libs/components/components.js';
 import { AppRoute } from '~/libs/enums/enums.js';
 import {
-  useAppDebounce,
   useAppDispatch,
   useAppForm,
   useAppSelector,
   useCallback,
   useEffect,
-  useRef,
 } from '~/libs/hooks/hooks.js';
-import { type ProjectSearchRequestDto } from '~/packages/projects/projects.js';
+import { type ProjectGetAllParametersDto } from '~/packages/projects/projects.js';
 import { actions as projectActions } from '~/slices/projects/projects.js';
 
 import { ProjectCard } from './components/project-card/project-card.js';
+import { DEFAULT_PROJECT_SEARCH_PAYLOAD } from './libs/constants.js';
+import { initDebounce } from './libs/helpers/init-debounce-helper.js';
 import styles from './styles.module.scss';
 
 const MyProjects: React.FC = () => {
@@ -26,32 +26,22 @@ const MyProjects: React.FC = () => {
     projects: state.projects.projects,
   }));
 
-  const { control, errors, handleSubmit } = useAppForm<ProjectSearchRequestDto>(
-    {
-      defaultValues: { search: '' },
+  const { control, errors, handleSubmit } =
+    useAppForm<ProjectGetAllParametersDto>({
+      defaultValues: DEFAULT_PROJECT_SEARCH_PAYLOAD,
       mode: 'onChange',
+    });
+
+  const handleSearch = useCallback(
+    (data: ProjectGetAllParametersDto): void => {
+      void dispatch(projectActions.getUserProjects({ query: data.search }));
     },
+    [dispatch],
   );
 
-  const previousQueryReference = useRef('');
-
-  const handleSearch = (data: ProjectSearchRequestDto): void => {
-    const query = data.search;
-
-    if (query !== previousQueryReference.current) {
-      void dispatch(projectActions.getUserProjects(query));
-      previousQueryReference.current = query;
-    }
-  };
-
-  const debouncedHandleSubmit = useAppDebounce(handleSubmit(handleSearch), 500);
-
-  const handleFormChange = useCallback(
-    (event_: React.BaseSyntheticEvent): void => {
-      void debouncedHandleSubmit(event_);
-    },
-    [debouncedHandleSubmit],
-  );
+  const handleFormChange = initDebounce((event_: React.BaseSyntheticEvent) => {
+    void handleSubmit(handleSearch)(event_);
+  }, 500);
 
   const hasProjects = projects.length > 0;
 
