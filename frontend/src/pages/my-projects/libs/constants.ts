59fba2aefb2ec88d6cd2c4304b4ce10455b54b75
Mariia <ma.gaiduchenko@gmail.com>
2023-03-30 18:18:00 +0300

    sl-88: + search payload constant

---
+++ frontend/src/pages/my-projects/libs/constants.ts
@@ -0,0 +1,7 @@
+import { type ProjectGetAllParametersDto } from '~/packages/projects/projects.js';
+
+const DEFAULT_PROJECT_SEARCH_PAYLOAD: ProjectGetAllParametersDto = {
+  search: '',
+};
+
+export { DEFAULT_PROJECT_SEARCH_PAYLOAD };
