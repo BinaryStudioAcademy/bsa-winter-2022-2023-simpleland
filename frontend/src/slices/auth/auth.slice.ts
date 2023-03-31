9861a3b4306e50555fe5b502723de193c10ab51d
Dima Popovych <dimapopovych.w@gmail.com>
2023-03-31 18:44:56 +0300

    sl-50: * grouped cases in auth slice

--- frontend/src/slices/auth/auth.slice.ts
+++ frontend/src/slices/auth/auth.slice.ts
@@ -1,4 +1,4 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice, isAnyOf } from '@reduxjs/toolkit';
 
 import { DataStatus } from '~/libs/enums/enums.js';
 import { type ValueOf } from '~/libs/types/types.js';
@@ -22,27 +22,45 @@ const { reducer, actions, name } = createSlice({
   name: 'auth',
   reducers: {},
   extraReducers(builder) {
-    builder.addCase(signIn.fulfilled, (state, action) => {
-      state.user = action.payload;
-    });
-    builder.addCase(signUp.fulfilled, (state, action) => {
-      state.user = action.payload;
-    });
     builder.addCase(logout.fulfilled, (state) => {
       state.currentUserDataStatus = DataStatus.IDLE;
       state.user = null;
     });
-    builder.addCase(getCurrentUser.fulfilled, (state, action) => {
-      state.user = action.payload;
-      state.currentUserDataStatus = DataStatus.FULFILLED;
-    });
-    builder.addCase(usersActions.updateUser.fulfilled, (state, action) => {
-      state.user = action.payload;
-    });
-    builder.addCase(
-      usersActions.updateUserAvatar.fulfilled,
+    builder.addMatcher(
+      isAnyOf(
+        signIn.pending,
+        signUp.pending,
+        getCurrentUser.pending,
+        usersActions.updateUser.pending,
+        usersActions.updateUserAvatar.pending,
+      ),
+      (state) => {
+        state.currentUserDataStatus = DataStatus.PENDING;
+      },
+    );
+    builder.addMatcher(
+      isAnyOf(
+        signIn.fulfilled,
+        signUp.fulfilled,
+        getCurrentUser.fulfilled,
+        usersActions.updateUser.fulfilled,
+        usersActions.updateUserAvatar.fulfilled,
+      ),
       (state, action) => {
+        state.currentUserDataStatus = DataStatus.FULFILLED;
         state.user = action.payload;
+      },
+    );
+    builder.addMatcher(
+      isAnyOf(
+        signIn.rejected,
+        signUp.rejected,
+        getCurrentUser.rejected,
+        usersActions.updateUser.rejected,
+        usersActions.updateUserAvatar.rejected,
+      ),
+      (state) => {
+        state.currentUserDataStatus = DataStatus.REJECTED;
       },
     );
   },
