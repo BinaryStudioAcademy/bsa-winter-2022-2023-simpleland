fabe554fcd31bbc5186eaa096be86c66a43950b7
jaroslawtt <yaroslavlogan61907@gmail.com>
2023-04-01 13:49:29 +0300

    sl-99: * updated the way hook handles optional validation schema

--- frontend/src/libs/hooks/use-app-form/use-app-form.hook.ts
+++ frontend/src/libs/hooks/use-app-form/use-app-form.hook.ts
@@ -4,45 +4,56 @@ import {
   type DeepPartial,
   type FieldErrors,
   type FieldValues,
+  type Mode,
   type UseFormHandleSubmit,
+  type UseFormProps,
   type UseFormReset,
 } from 'react-hook-form';
 import { useForm } from 'react-hook-form';
 
 import { type ValidationSchema } from '~/libs/types/types.js';
-import { emptyValidationSchema } from '~/libs/validation-schemas/validation-schemas.js';
 
 type Parameters<T extends FieldValues = FieldValues> = {
   defaultValues: DeepPartial<T>;
   validationSchema?: ValidationSchema;
+  mode: Mode;
 };
 
 type ReturnValue<T extends FieldValues = FieldValues> = {
   control: Control<T, null>;
   errors: FieldErrors<T>;
   handleSubmit: UseFormHandleSubmit<T>;
-  handleReset: UseFormReset<T>;
+  reset: UseFormReset<T>;
 };
 
 const useAppForm = <T extends FieldValues = FieldValues>({
   validationSchema,
   defaultValues,
+  mode,
 }: Parameters<T>): ReturnValue<T> => {
+  let parameters: UseFormProps<T> = {
+    mode,
+    defaultValues,
+  };
+
+  if (validationSchema) {
+    parameters = {
+      ...parameters,
+      resolver: joiResolver(validationSchema),
+    };
+  }
   const {
     control,
     handleSubmit,
     formState: { errors },
-    reset: handleReset,
-  } = useForm<T>({
-    defaultValues,
-    resolver: joiResolver(validationSchema ?? emptyValidationSchema),
-  });
+    reset,
+  } = useForm<T>(parameters);
 
   return {
     control,
     errors,
     handleSubmit,
-    handleReset,
+    reset,
   };
 };
 
