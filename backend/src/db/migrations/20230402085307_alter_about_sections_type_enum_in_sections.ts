65388ed4b7baaf260f5a262ec292912cec5e0397
Mariia <ma.gaiduchenko@gmail.com>
2023-04-02 13:33:09 +0300

    sl-138: + about sections type to sections table

---
+++ backend/src/db/migrations/20230402085307_alter_about_sections_type_enum_in_sections.ts
@@ -0,0 +1,38 @@
+import { type Knex } from 'knex';
+
+const TABLE_NAME = 'sections';
+
+const COLUMN_NAME = 'type';
+
+const ENUM_NAME = 'enum_about_section_sections_type';
+
+const SECTION_TYPES = ['header', 'footer', 'main'];
+
+const UPDATED_SECTION_TYPES = [...SECTION_TYPES, 'about_section'];
+
+async function up(knex: Knex): Promise<void> {
+  await knex.schema.alterTable(TABLE_NAME, (table) => {
+    table.dropColumn(COLUMN_NAME);
+  });
+
+  return await knex.schema.alterTable(TABLE_NAME, (table) => {
+    table.enum(COLUMN_NAME, UPDATED_SECTION_TYPES, {
+      useNative: true,
+      enumName: ENUM_NAME,
+    });
+  });
+}
+
+async function down(knex: Knex): Promise<void> {
+  await knex.schema.alterTable(TABLE_NAME, (table) => {
+    table.dropColumn(COLUMN_NAME);
+  });
+
+  await knex.schema.alterTable(TABLE_NAME, (table) => {
+    table.enum(COLUMN_NAME, SECTION_TYPES).notNullable();
+  });
+
+  return await knex.schema.raw(`DROP TYPE IF EXISTS ${ENUM_NAME} CASCADE;`);
+}
+
+export { down, up };
