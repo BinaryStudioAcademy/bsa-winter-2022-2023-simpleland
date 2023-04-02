603ce66f432f68f920c6bf6d50941b904a4727a7
Dima Popovych <dimapopovych.w@gmail.com>
2023-04-02 18:51:58 +0300

    sl-141: * enum_section_type add feedback section

---
+++ backend/src/db/migrations/20230402091558_add_feedback_to_enum_sections_type.ts
@@ -0,0 +1,17 @@
+import { type Knex } from 'knex';
+
+const ENUM_NAME = 'enum_sections_type';
+
+const SECTION_TYPE = 'feedback';
+
+async function up(knex: Knex): Promise<void> {
+  return await knex.schema.raw(
+    `ALTER TYPE ${ENUM_NAME} ADD VALUE IF NOT EXISTS '${SECTION_TYPE}'`,
+  );
+}
+
+function down(knex: Knex): void {
+  return;
+}
+
+export { down, up };
