6dfb4ff9a5e8145e483bb4690fe45a0cdfd4818b
jaroslawtt <yaroslavlogan61907@gmail.com>
2023-04-03 00:43:44 +0300

    sl-144: * renamed toggle func and typed state

--- frontend/src/libs/hooks/use-toggle/use-toggle.hook.ts
+++ frontend/src/libs/hooks/use-toggle/use-toggle.hook.ts
@@ -1,13 +1,16 @@
 import { useCallback, useState } from '~/libs/hooks/hooks.js';
 
-type ReturnValue = [value: boolean, toggleValue: () => void];
+type ReturnValue = [value: boolean, handleValueToggle: () => void];
 
 const useToggle = (initialValue: boolean): ReturnValue => {
-  const [value, setValue] = useState(initialValue);
+  const [value, setValue] = useState<boolean>(initialValue);
 
-  const toggleValue = useCallback(() => setValue(!value), [value, setValue]);
+  const handleValueToggle = useCallback(
+    () => setValue(!value),
+    [value, setValue],
+  );
 
-  return [value, toggleValue];
+  return [value, handleValueToggle];
 };
 
 export { useToggle };
