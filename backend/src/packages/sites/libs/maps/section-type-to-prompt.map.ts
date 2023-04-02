158ce2f10fb61b723d0c603aadfe2b1c5ce9491d
Mariia <ma.gaiduchenko@gmail.com>
2023-04-02 16:04:13 +0300

    sl-138: - new line break in description

--- backend/src/packages/sites/libs/maps/section-type-to-prompt.map.ts
+++ backend/src/packages/sites/libs/maps/section-type-to-prompt.map.ts
@@ -17,7 +17,7 @@ const SectionTypeToPrompt: Record<
   },
   [SectionType.ABOUT]: {
     EXAMPLE:
-      'title: About Studio\ndescription: Interiart is an award-winning architecture and interior design practice based in NYC. We work internationally on projects of residential & commercial interior design that require a creative approach. Our talented and experienced designers leverage their knowledge and expertise to create unique and comfortable interiors for you.\nOur team knows that interior design can be stressful for the client and we do our best to make it as easy as possible. We listen to your needs, ideas, and inputs. And most importantly, we make it exciting and enjoyable for our clients.',
+      'title: About Studio\ndescription: Interiart is an award-winning architecture and interior design practice based in NYC. We work internationally on projects of residential & commercial interior design that require a creative approach. Our talented and experienced designers leverage their knowledge and expertise to create unique and comfortable interiors for you. Our team knows that interior design can be stressful for the client and we do our best to make it as easy as possible. We listen to your needs, ideas, and inputs. And most importantly, we make it exciting and enjoyable for our clients.',
     REQUEST: 'Generate title for about section, description for about section.',
   },
   [SectionType.FOOTER]: {
