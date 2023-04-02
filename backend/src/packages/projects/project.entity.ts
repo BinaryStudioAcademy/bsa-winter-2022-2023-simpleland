908973f69fb8db7904689d8480abb9202d1a3bfc
jaroslawtt <yaroslavlogan61907@gmail.com>
2023-04-03 00:22:48 +0300

    sl-99: + extended model and entity to consist fields image id and url

--- backend/src/packages/projects/project.entity.ts
+++ backend/src/packages/projects/project.entity.ts
@@ -3,37 +3,53 @@ import { type IEntity } from '~/libs/interfaces/interfaces.js';
 class ProjectEntity implements IEntity {
   private 'id': number | null;
 
-  private 'name': string;
+  private 'name': string | null;
 
-  private 'userId': number;
+  private 'userId': number | null;
+
+  private 'imageId': number | null;
+
+  private 'imageUrl': string | null;
 
   private constructor({
     id,
     name,
     userId,
+    imageId,
+    imageUrl,
   }: {
     id: number | null;
-    name: string;
-    userId: number;
+    name: string | null;
+    userId: number | null;
+    imageId: number | null;
+    imageUrl: string | null;
   }) {
     this.id = id;
     this.name = name;
     this.userId = userId;
+    this.imageId = imageId;
+    this.imageUrl = imageUrl;
   }
 
   public static initialize({
     id,
     name,
     userId,
+    imageId,
+    imageUrl,
   }: {
-    id: number;
-    name: string;
-    userId: number;
+    id: number | null;
+    name: string | null;
+    userId: number | null;
+    imageId: number | null;
+    imageUrl: string | null;
   }): ProjectEntity {
     return new ProjectEntity({
       id,
       name,
       userId,
+      imageId,
+      imageUrl,
     });
   }
 
@@ -48,6 +64,8 @@ class ProjectEntity implements IEntity {
       id: null,
       name,
       userId,
+      imageId: null,
+      imageUrl: null,
     });
   }
 
@@ -55,11 +73,13 @@ class ProjectEntity implements IEntity {
     id: number;
     name: string;
     userId: number;
+    imageUrl: string | null;
   } {
     return {
       id: this.id as number,
-      name: this.name,
-      userId: this.userId,
+      name: this.name as string,
+      userId: this.userId as number,
+      imageUrl: this.imageUrl,
     };
   }
 
@@ -68,8 +88,18 @@ class ProjectEntity implements IEntity {
     userId: number;
   } {
     return {
-      name: this.name,
-      userId: this.userId,
+      name: this.name as string,
+      userId: this.userId as number,
+    };
+  }
+
+  public toUserImage(): {
+    id: number;
+    imageId: number;
+  } {
+    return {
+      id: this.id as number,
+      imageId: this.imageId as number,
     };
   }
 }
