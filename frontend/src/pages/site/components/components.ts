e388d50f041e4e26259efa22303a1c1a59102704
Mariia <ma.gaiduchenko@gmail.com>
2023-04-02 13:42:11 +0300

    sl-138: + about section component

--- frontend/src/pages/site/components/components.ts
+++ frontend/src/pages/site/components/components.ts
@@ -1,3 +1,4 @@
+export { AboutSection } from './about-section/about-section.js';
 export { Footer } from './footer/footer.js';
 export { Header } from './header/header.js';
 export { Main } from './main/main.js';
