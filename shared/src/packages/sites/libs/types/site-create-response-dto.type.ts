38467cffb843f56b80bfc07bc40e045884f4fe9e
Alyona Pisotska <pisotska.o@gmail.com>
2023-04-04 18:32:17 +0300

    sl-95: + image for types

--- shared/src/packages/sites/libs/types/site-create-response-dto.type.ts
+++ shared/src/packages/sites/libs/types/site-create-response-dto.type.ts
@@ -2,6 +2,7 @@ type SiteCreateResponseDto = {
   id: number;
   name: string;
   publishedUrl: string | null;
+  image: string;
 };
 
 export { type SiteCreateResponseDto };
--- shared/src/packages/sites/libs/types/site-get-all-item-response-dto.type.ts
+++ shared/src/packages/sites/libs/types/site-get-all-item-response-dto.type.ts
@@ -2,6 +2,7 @@ type SiteGetAllItemResponseDto = {
   id: number;
   name: string;
   publishedUrl: string | null;
+  image: string;
 };
 
 export { type SiteGetAllItemResponseDto };
