72aa3d8ed29bd2285af68daa646d51a7df970d37
Alyona Pisotska <pisotska.o@gmail.com>
2023-04-04 12:37:09 +0300

    sl-74: * run prettify

--- frontend/src/libs/components/tooltip/tooltip.tsx
+++ frontend/src/libs/components/tooltip/tooltip.tsx
@@ -3,7 +3,7 @@ import 'react-tooltip/dist/react-tooltip.css';
 import { Tooltip as ReactTooltip } from 'react-tooltip';
 
 const Tooltip: React.FC = () => {
-  return <ReactTooltip id='app-main-tooltip' />;
+  return <ReactTooltip id="app-main-tooltip" />;
 };
 
 export { Tooltip };
