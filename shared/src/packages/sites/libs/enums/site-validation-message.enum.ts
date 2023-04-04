5ceb3b2defb837b7b55d2f512a8ef531e7f7cfd7
jaroslawtt <yaroslavlogan61907@gmail.com>
2023-04-04 13:17:04 +0300

    sl-173: + added messages for tone errors

--- shared/src/packages/sites/libs/enums/site-validation-message.enum.ts
+++ shared/src/packages/sites/libs/enums/site-validation-message.enum.ts
@@ -2,6 +2,8 @@ const SiteValidationMessage = {
   SITE_NAME_REQUIRE: 'Project name is required',
   SITE_NAME_WRONG: 'Project name is wrong',
   SITE_NAME_PATTERN: 'Incorrect project name',
+  SITE_TONE_REQUIRE: 'Tone of voice is required',
+  SITE_TONE_PATTERN: 'Incorrect type if voice tone',
 };
 
 export { SiteValidationMessage };
