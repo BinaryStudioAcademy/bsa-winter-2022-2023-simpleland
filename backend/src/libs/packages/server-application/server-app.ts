62cb84cc5ad613e3ee98b0bbdc7414e0e033a5ce
Mariia <ma.gaiduchenko@gmail.com>
2023-04-04 09:41:39 +0300

    sl-88: + query params

--- backend/src/libs/packages/server-application/server-app.ts
+++ backend/src/libs/packages/server-application/server-app.ts
@@ -69,6 +69,7 @@ class ServerApp implements IServerApp {
       schema: {
         body: validation?.body,
         params: validation?.params,
+        querystring: validation?.query,
       },
     });
 
