dc5a6ac8e18b0fb46d3a794f00016d94b271c147
Alyona Pisotska <pisotska.o@gmail.com>
2023-04-04 18:31:17 +0300

    sl-95: + migration

---
+++ backend/src/db/migrations/20230404135108_add_image_to_sites_table.ts
@@ -0,0 +1,19 @@
+import { type Knex } from 'knex';
+
+const TABLE_NAME = 'sites';
+
+const COLUMN_NAME = 'image';
+
+function up(knex: Knex): Promise<void> {
+  return knex.schema.alterTable(TABLE_NAME, (table) => {
+    table.string(COLUMN_NAME).defaultTo('');
+  });
+}
+
+function down(knex: Knex): Promise<void> {
+  return knex.schema.alterTable(TABLE_NAME, (table) => {
+    table.dropColumn(COLUMN_NAME);
+  });
+}
+
+export { down, up };
