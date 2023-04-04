59f1a7149b1e4eb40cc2dc9cd15e3ed49de9f93f
Mariia <ma.gaiduchenko@gmail.com>
2023-04-04 20:11:27 +0300

    sl-143: + project type in val schema

--- shared/src/packages/projects/libs/enums/project-validation-message.enum.ts
+++ shared/src/packages/projects/libs/enums/project-validation-message.enum.ts
@@ -1,6 +1,8 @@
 const ProjectValidationMessage = {
   NAME_REQUIRE: 'Name is required',
   NAME_WRONG: 'Name is wrong',
+  TYPE_REQUIRE: 'Type is required',
+  TYPE_WRONG: 'Type is wrong',
   USER_ID_REQUIRE: 'User ID is required',
   USER_ID_WRONG: 'User ID is wrong',
   FILTER_NAME_WRONG: 'The entered name is wrong',
--- shared/src/packages/projects/libs/validation-schemas/project-create.validation-schema.ts
+++ shared/src/packages/projects/libs/validation-schemas/project-create.validation-schema.ts
@@ -1,5 +1,7 @@
 import joi from 'joi';
 
+import { ProjectType } from '~/packages/projects/libs/enums/enums.js';
+
 import { ProjectValidationMessage } from '../enums/enums.js';
 import { type ProjectCreateRequestDto } from '../types/types.js';
 
@@ -8,6 +10,14 @@ const projectCreate = joi.object<ProjectCreateRequestDto, true>({
     'string.name': ProjectValidationMessage.NAME_WRONG,
     'string.empty': ProjectValidationMessage.NAME_REQUIRE,
   }),
+  type: joi
+    .string()
+    .valid(...Object.keys(ProjectType))
+    .required()
+    .messages({
+      'any.only': ProjectValidationMessage.TYPE_WRONG,
+      'any.required': ProjectValidationMessage.TYPE_REQUIRE,
+    }),
 });
 
 export { projectCreate };
