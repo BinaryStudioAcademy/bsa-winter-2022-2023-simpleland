98567a8ebf2bf12d6c992e6c16689be19a7f4d9a
Dima Popovych <dimapopovych.w@gmail.com>
2023-04-05 17:11:19 +0300

    sl-100: + subscription migrations

---
+++ backend/src/db/migrations/20230405083951_add_subscription_table.ts
@@ -0,0 +1,31 @@
+import { type Knex } from 'knex';
+
+const TABLE_NAME = 'subscriptions';
+
+const ColumnName = {
+  ID: 'id',
+  SUBSCRIPTION_END: 'subscription_end',
+  CREATED_AT: 'created_at',
+  UPDATED_AT: 'updated_at',
+};
+
+function up(knex: Knex): Promise<void> {
+  return knex.schema.createTable(TABLE_NAME, (table) => {
+    table.increments(ColumnName.ID).primary();
+    table.dateTime(ColumnName.SUBSCRIPTION_END).notNullable();
+    table
+      .dateTime(ColumnName.CREATED_AT)
+      .notNullable()
+      .defaultTo(knex.fn.now());
+    table
+      .dateTime(ColumnName.UPDATED_AT)
+      .notNullable()
+      .defaultTo(knex.fn.now());
+  });
+}
+
+function down(knex: Knex): Promise<void> {
+  return knex.schema.dropTableIfExists(TABLE_NAME);
+}
+
+export { down, up };
