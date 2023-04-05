3b8cedb211e0affccbdb00cba9b1a302a5f16818
jaroslawtt <yaroslavlogan61907@gmail.com>
2023-04-05 10:06:56 +0300

    sl-94: * set official tone as default one for site

--- frontend/src/pages/start/libs/constants.ts
+++ frontend/src/pages/start/libs/constants.ts
@@ -1,3 +1,4 @@
+import { SiteToneType } from '~/packages/sites/libs/enums/enums.js';
 import { type SiteCreateRequestDto } from '~/packages/sites/sites.js';
 
 const ONE_STEP_LENGTH = 1 as const;
@@ -5,7 +6,7 @@ const ONE_STEP_LENGTH = 1 as const;
 const DEFAULT_SITE_PAYLOAD: SiteCreateRequestDto = {
   name: '',
   industry: 'Programming courses', // TODO: replace
-  tone: '',
+  tone: SiteToneType.OFFICIAL,
 } as const;
 
 export { DEFAULT_SITE_PAYLOAD, ONE_STEP_LENGTH };
