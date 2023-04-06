9ed31f742f0050ca96836346b6418f2b46ae5531
vbazhynov <vladbazhynov@gmail.com>
2023-04-06 16:49:41 +0300

    sl-90: - delete debounce and unnesessery type files

--- shared/src/packages/sites/libs/types/sites-search-request-dto.type.ts
+++
@@ -1,5 +0,0 @@
-type SitesFilterQueryDto = {
-  pattern: string;
-};
-
-export { type SitesFilterQueryDto };
--- shared/src/packages/sites/libs/validation-schemas/site-search.validation-schema.ts
+++
@@ -1,9 +0,0 @@
-import joi from 'joi';
-
-import { type SitesFilterQueryDto } from '../types/types.js';
-
-const siteSearch = joi.object<SitesFilterQueryDto, true>({
-  pattern: joi.string().allow(null, ''),
-});
-
-export { siteSearch };
