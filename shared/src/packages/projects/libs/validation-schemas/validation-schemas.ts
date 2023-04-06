be884154295ed7dd289bbdc22725320493fbf587
Mariia <ma.gaiduchenko@gmail.com>
2023-04-06 20:22:24 +0300

    sl-93: + project update val schema

--- shared/src/packages/projects/libs/validation-schemas/validation-schemas.ts
+++ shared/src/packages/projects/libs/validation-schemas/validation-schemas.ts
@@ -1,2 +1,3 @@
 export { projectCreate } from './project-create.validation-schema.js';
 export { projectFilter } from './project-filter.validation-schema.js';
+export { projectUpdate } from './project-update.validation-schema.js';
