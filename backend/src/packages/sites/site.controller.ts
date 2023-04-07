debc980359add77e3a6ec80663ccea27609c5f3b
Mariia <ma.gaiduchenko@gmail.com>
2023-04-07 21:48:01 +0300

    sl-222: + category in swagger doc

--- backend/src/packages/sites/site.controller.ts
+++ backend/src/packages/sites/site.controller.ts
@@ -164,6 +164,15 @@ class SiteController extends Controller {
    *             properties:
    *              name:
    *                type: string
+   *              category:
+   *                type: string
+   *                enum:
+   *                  - e-commercial
+   *                  - business
+   *                  - blog
+   *                  - portfolio
+   *                  - personal
+   *                  - nonprofit
    *              industry:
    *                type: string
    *              tone:
