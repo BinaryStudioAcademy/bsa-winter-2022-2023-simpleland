7041d6346e777eb47bb652510619a6b8fe8abe4b
jaroslawtt <yaroslavlogan61907@gmail.com>
2023-04-10 17:16:12 +0300

    sl-160: + added dispatching fetching site, added rendering button

--- frontend/src/pages/site/components/main/main.tsx
+++ frontend/src/pages/site/components/main/main.tsx
@@ -5,16 +5,19 @@ import styles from './styles.module.scss';
 
 type Properties = {
   content: SiteMainContent;
+  renderButton: () => JSX.Element;
 };
 
 const Main: React.FC<Properties> = ({
   content: { description, title, picture },
+  renderButton,
 }: Properties) => {
   return (
     <div className={styles['main']}>
       <div className={styles['main-content']}>
         <div className={styles['main-content-left']} />
         <div className={styles['main-content-right']}>
+          {renderButton()}
           <div className={styles['main-title']}>{title}</div>
           <div className={styles['main-description']}>{description}</div>
           <button className={styles['main-button']}>Get started</button>
--- frontend/src/pages/site/site.tsx
+++ frontend/src/pages/site/site.tsx
@@ -1,12 +1,20 @@
-import { Loader, PageLayout } from '~/libs/components/components.js';
-import { DataStatus } from '~/libs/enums/enums.js';
+import {
+  Icon,
+  Link,
+  Loader,
+  PageLayout,
+} from '~/libs/components/components.js';
+import { AppRoute, DataStatus } from '~/libs/enums/enums.js';
+import { configureString } from '~/libs/helpers/helpers.js';
 import {
   useAppDispatch,
   useAppSelector,
+  useCallback,
   useEffect,
   useParams,
   useTitle,
 } from '~/libs/hooks/hooks.js';
+import { type ValueOf } from '~/libs/types/types.js';
 import {
   type SiteAboutContent,
   type SiteFeedbackContent,
@@ -18,6 +26,7 @@ import {
   SectionType,
 } from '~/packages/sections/sections.js';
 import { actions as sectionsActions } from '~/slices/sections/sections.js';
+import { actions as sitesActionss } from '~/slices/sites/sites.js';
 
 import {
   About,
@@ -34,16 +43,44 @@ const Site: React.FC = () => {
   const dispatch = useAppDispatch();
   useTitle('My site');
 
-  const { sections, status } = useAppSelector((state) => ({
+  const { sections, status, siteName, projectId } = useAppSelector((state) => ({
     sections: state.sections.sections,
     status: state.sections.dataStatus,
+    siteName: state.sites.currentSite?.name,
+    projectId: state.sites.currentSite?.projectId,
   }));
 
   const { siteId } = useParams() as { siteId: string };
 
   useEffect(() => {
-    void dispatch(sectionsActions.getSiteSections({ siteId: Number(siteId) }));
+    void dispatch(sectionsActions.getSiteSections({ siteId: Number(siteId) }))
+      .unwrap()
+      .then(() => {
+        void dispatch(sitesActionss.getCurrentSite({ id: Number(siteId) }));
+      });
   }, [dispatch, siteId]);
+
+  const renderReturnButton = useCallback((): JSX.Element => {
+    return (
+      <div className={styles['button-wrapper']}>
+        <div>
+          <Link
+            to={configureString<ValueOf<typeof AppRoute>>(
+              AppRoute.PROJECTS_$PROJECT_ID_SITES,
+              {
+                projectId,
+              },
+            )}
+          >
+            <span className={styles['link-to-projects']}>
+              <Icon iconName="arrowLeft" className={styles['back-icon']} />
+            </span>
+          </Link>
+        </div>
+        <h2>{siteName}</h2>
+      </div>
+    );
+  }, [siteName, projectId]);
 
   if (status === DataStatus.PENDING) {
     return (
@@ -60,7 +97,13 @@ const Site: React.FC = () => {
           return <Header content={content as SiteHeaderContent} key={type} />;
         }
         case SectionType.MAIN: {
-          return <Main content={content as SiteMainContent} key={type} />;
+          return (
+            <Main
+              content={content as SiteMainContent}
+              key={type}
+              renderButton={renderReturnButton}
+            />
+          );
         }
         case SectionType.ABOUT: {
           return <About content={content as SiteAboutContent} key={type} />;
