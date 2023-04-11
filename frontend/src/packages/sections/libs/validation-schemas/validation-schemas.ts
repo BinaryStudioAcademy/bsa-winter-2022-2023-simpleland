7eb0b20b86dd47edc3d238b8225fd36775fb2127
vbazhynov <vladbazhynov@gmail.com>
2023-04-11 11:14:21 +0300

    sl-177: * fix imports

--- frontend/src/packages/sections/libs/validation-schemas/validation-schemas.ts
+++ frontend/src/packages/sections/libs/validation-schemas/validation-schemas.ts
@@ -1,2 +1,4 @@
-export { siteHeaderUpdateContentValidationSchema } from 'shared/build/index.js';
-export { sitePortfolioUpdateContentValidationSchema } from 'shared/build/index.js';
+export {
+  siteHeaderUpdateContentValidationSchema,
+  sitePortfolioUpdateContentValidationSchema,
+} from 'shared/build/index.js';
