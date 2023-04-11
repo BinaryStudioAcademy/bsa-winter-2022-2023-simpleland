79c972ada4260698ec1cc2c8ce64d5513db19351
jaroslawtt <yaroslavlogan61907@gmail.com>
2023-04-11 17:12:38 +0300

    sl-178: + extended dto, validation schema, added enum values

--- shared/src/packages/sections/libs/types/site-feedback-update-content-dto.type.ts
+++ shared/src/packages/sections/libs/types/site-feedback-update-content-dto.type.ts
@@ -1,5 +1,11 @@
 type SiteFeedbackUpdateContentDto = {
   title: string;
+  cards: {
+    photo: string;
+    name: string;
+    profession: string;
+    feedback: string;
+  }[];
 };
 
 export { type SiteFeedbackUpdateContentDto };
