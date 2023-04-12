d61478578ccfe2bb1fcd19968463a90612e346b1
jaroslawtt <yaroslavlogan61907@gmail.com>
2023-04-12 14:42:38 +0300

    sl-274: + removed unnecessary styles and using it

--- frontend/src/pages/site/components/service/components/service-card/service-card.tsx
+++ frontend/src/pages/site/components/service/components/service-card/service-card.tsx
@@ -1,7 +1,6 @@
 import { type Control, type FieldErrors } from 'react-hook-form';
 
 import { Input } from '~/libs/components/components.js';
-import { getValidClassNames } from '~/libs/helpers/helpers.js';
 import { type SiteServiceUpdateContentDto } from '~/packages/sections/sections.js';
 
 import styles from './styles.module.scss';
@@ -41,10 +40,6 @@ const ServiceCard: React.FC<Properties> = ({
               name={`cards.${cardIndex}.title`}
               label="Service title"
               isLabelVisuallyHidden
-              className={getValidClassNames(
-                styles['edit-service-card'],
-                styles['service-card-title'],
-              )}
               onBlur={onSectionUpdate}
               isInline
             />
@@ -60,10 +55,6 @@ const ServiceCard: React.FC<Properties> = ({
               name={`cards.${cardIndex}.description`}
               label="Service description"
               isLabelVisuallyHidden
-              className={getValidClassNames(
-                styles['edit-service-card'],
-                styles['service-card-description'],
-              )}
               rows={7}
               onBlur={onSectionUpdate}
               isInline
